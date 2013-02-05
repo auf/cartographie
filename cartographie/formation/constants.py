a512391b590b4f2e2b3d0410ce50251a92f57f21
Bernard Chhun <bernard.chhun@savoirfairelinux.com>
2013-02-05 15:54:20 -0500

    5335 constantes

---
+++ cartographie/formation/constants.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+
+
+class SessionConstants(object):
+    # nom de la variable session pour contenir l'id de l'établissement courant
+    eta_id = "espace_formation_etablissement_id"
+
+session_const = SessionConstants()
