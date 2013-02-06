d5f0999a8af806900684a19ff1631477142ed322
Bernard Chhun <bernard.chhun@savoirfairelinux.com>
2013-02-06 00:05:14 -0500

    5332: commande pour générer des tokens pour tous les établissements membres.

--- cartographie/formation/constants.py
+++ cartographie/formation/constants.py
@@ -2,7 +2,13 @@
 
 
 class SessionConstants(object):
-    # nom de la variable session pour contenir l'id de l'établissement courant
+    """
+    Classe servant de conteneur pour les noms de variable Session.
+
+    L'idée est de pouvoir les modifier en une seul et même place
+    et de ne pas passer une string à l'objet request.session
+    """
+
     eta_id = "espace_formation_etablissement_id"
     erreur = "espace_formation_erreur"
 
---
+++ cartographie/formation/management/commands/jetonizer.py
@@ -0,0 +1,28 @@
+#coding: utf-8
+
+from django.core.management.base import BaseCommand
+
+from auf.django.references import models as ref
+from formation.models import Acces
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        etabs = ref.Etablissements.objects.filter(membre=True).all()
+
+        for etab in etabs:
+            self.stdout.write("Verification de l'établissement: %s" % etab)
+
+            acces_avec_token = Acces.filter(etablissement=etab).all()
+
+            if len(acces_avec_token) == 0:
+                self.stdout.write("Pas de jeton d'acces")
+
+                nouvel_acces = Acces()
+                nouvel_acces.etablissement = etab
+                nouvel_acces.active = True
+                nouvel_acces.generer_token()
+                nouvel_acces.save()
+
+            else:
+                self.stdout.write("Possede un jeton d'acces")
