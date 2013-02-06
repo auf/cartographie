6d00d2d644e4cabd828e1fb82f518a7fbda279b6
Bernard Chhun <bernard.chhun@savoirfairelinux.com>
2013-02-06 13:49:47 -0500

    # 5355 cleanup des imports

--- cartographie/formation/admin.py
+++ cartographie/formation/admin.py
@@ -1,8 +1,7 @@
 #coding: utf-8
 
 from django import forms
-from django.contrib.auth.admin import \
-     UserAdmin as DjangoUserAdmin, GroupAdmin as DjangoGroupAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.contrib.auth.forms import UserChangeForm as DjangoUserForm
 
 from auf.django.permissions.forms import make_global_permissions_form
