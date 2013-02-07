2aab4ed2038e3c12ea2b48492d475726472c4c07
Bernard Chhun <bernard.chhun@savoirfairelinux.com>
2013-02-07 08:46:39 -0500

    petit cleanup des urls. ajout du logo de l'AUF pour le logo de la page de formulaire

--- cartographie/formation/urls.py
+++ cartographie/formation/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns("cartographie.formation.views",
-    url(r"^connexion/(?P<token>\w+)$", "connexion", name="formation_connexion"),
+    url(r"^connexion/(?P<token>\w+)$", "connexion",
+        name="formation_connexion"),
     url(r"^erreur$", "erreur", name="formation_erreur"),
     url(r"^liste$", "liste", name="formation_liste"),
     url(r"^ajouter$", "ajouter", name="formation_ajouter"),
