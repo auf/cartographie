d43a8ab0a969f8b26c00d48d1a9300801be98bd9
Bernard Chhun <bernard.chhun@savoirfairelinux.com>
2013-02-11 14:53:19 -0500

    # 5340: ajout de Chosen pour rendre les selects un peu plus user friendly

---
+++ cartographie/static/formation/js/site.js
@@ -0,0 +1,43 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+var AUF = {};
+
+AUF.formation = function(){
+    return {
+        init: function(){
+            console.log("AUF.formation.init()");
+
+            this.beautifulSelects();
+        },
+        beautifulSelects : function(){
+            console.log("AUF.formation.beautifulSelects()");
+
+            $(".form-auf-ajouter select").chosen();
+        }
+    }
+}();
+
+$(document).ready(function(){
+    AUF.formation.init();
+});
