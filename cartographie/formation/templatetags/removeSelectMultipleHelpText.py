abcfbb2096f391289efe2cc484b2f45c3afbc1db
bchhun <bernard.chhun@gmail.com>
2013-02-11 21:58:54 -0500

    5340 help text dans le formulaire

---
+++ cartographie/formation/templatetags/removeSelectMultipleHelpText.py
@@ -0,0 +1,15 @@
+#coding: utf-8
+
+from django import template
+from django.utils.translation import ugettext as _
+
+register = template.Library()
+
+
+@register.filter
+def removeSelectMultipleHelpText(value):
+    msg =  unicode(
+        _('Hold down "Control", or "Command" on a Mac, to select more than one.')
+    )
+
+    return value.replace(msg, "")
