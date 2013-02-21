b97822bfb8fabc67095824b3c47380cc1c2d5260
Bernard Chhun <bernard.chhun@savoirfairelinux.com>
2013-02-21 16:49:21 -0500

    #5667 liste des établissements d'un user connecté avec lien menant aux formations

--- cartographie/tableau_de_bord/viewModels/liste_etablissements.py
+++ cartographie/tableau_de_bord/viewModels/liste_etablissements.py
@@ -1,13 +1,23 @@
 #coding: utf-8
 
+from cartographie.formation.models import UserRole, Acces
+
 
 class ListeEtablissementsViewModel(object):
     menu_actif = None
+    liste_acces = None
 
     def __init__(self, request, menu_actif="liste_etablissements"):
         self.menu_actif = menu_actif
 
+        role = UserRole.objects.get(user=request.user)
+
+        self.liste_acces = Acces.objects.filter(
+            etablissement__region__in=role.regions.all()
+        ).order_by("etablissement__nom")
+
     def get_data(self):
         return {
-            "menu_actif": self.menu_actif
+            "menu_actif": self.menu_actif,
+            "liste_acces": self.liste_acces
         }
