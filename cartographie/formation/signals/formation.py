44c0acdbcbe99566119819a08b0a348714274c65
bchhun <bernard.chhun@gmail.com>
2013-02-28 14:45:45 -0500

    #5688: test d'envoi avec éditeur multiple. reformatage des messages car trop d'espace

--- cartographie/formation/signals/formation.py
+++ cartographie/formation/signals/formation.py
@@ -1,9 +1,12 @@
 #coding: utf-8
+
+from django.template import Context
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
-from django.template import Context
+from django.contrib.auth.models import User
 
 from cartographie.formation.constants import statuts_formation as STATUTS
+from cartographie.formation.models import Acces, UserRole
 
 
 def formation_is_valider(sender, instance, signal, *args, **kwargs):
@@ -17,24 +20,39 @@ def formation_is_valider(sender, instance, signal, *args, **kwargs):
         plaintext = get_template('cartographie/email/formation_validee.txt')
         htmly = get_template('cartographie/email/formation_validee.html')
 
-        # TODO: obtenir l'éditeur de la formation courante
-        # TODO: obtenir le super utilisateur
-        # TODO: obtenir le token
+        acces = Acces.objects.get(
+            etablissement__id=formation_courante.etablissement.id
+        )
 
         d = Context({
             'formation': formation_courante,
-            "token": "le token de l'établissement de la formation"
+            "token": acces.token
         })
 
         subject = u'Formation %i validée: %s' % (
             formation_courante.id, formation_courante.nom
         )
 
-        from_email = 'from@example.com'
-        to = 'to@example.com'
+        from_email = 'cartographie@auf.org'
+        to = []
+        bcc = []
+
+        # obtenir le(s) courriel(s) de(s) éditeur(s) de la formation courante
+        user_roles = UserRole.objects.filter(type="editeur").values_list(
+            "regions", "user__email"
+        )
+        for region_id, editeur_email in user_roles:
+            if region_id == formation_courante.etablissement.region.id:
+                if editeur_email:
+                    to.append(editeur_email)
+
+        # obtenir courriel du super utilisateur
+        for u in User.objects.filter(is_superuser=True):
+            if u.email:
+                bcc.append(u.email)
 
         text_content = plaintext.render(d)
         html_content = htmly.render(d)
-        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg = EmailMultiAlternatives(subject, text_content, from_email, to, bcc)
         msg.attach_alternative(html_content, "text/html")
         msg.send()
