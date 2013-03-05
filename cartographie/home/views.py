d36736fac14ee0ea354ce6861a51ab600e0d0a1a
Davin Baragiotta <davin.baragiotta@auf.org>
2013-03-04 19:36:09 -0500

    interfaces publiques : actuellement protégées

--- cartographie/home/views.py
+++ cartographie/home/views.py
@@ -1,10 +1,10 @@
 #coding: utf-8
 
-
-from django.template import RequestContext
-# from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 # from django.core.urlresolvers import reverse
+# from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, render
+from django.template import RequestContext
 
 from cartographie.formation.models import Formation
 
@@ -18,7 +18,7 @@ def accueil(request):
         "accueil.html", vm.get_data(), RequestContext(request)
     )
 
-#@login_required
+@login_required
 def formation_liste(request):
     formations = Formation.objects.all()
     c = {
@@ -26,7 +26,7 @@ def formation_liste(request):
     }
     return render(request, "formation_liste.html", c)
 
-#@login_required
+@login_required
 def formation_detail(request, id, slug=None):
     formation = Formation.objects.get(pk=id)
     c = {
