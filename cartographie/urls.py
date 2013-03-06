c2d9d85dcb6e2858a001abda878a0bd38c8ced0a
bchhun <bernard.chhun@gmail.com>
2013-03-06 10:05:00 -0500

    #5504 ajout des pages 404 et 500.

--- cartographie/urls.py
+++ cartographie/urls.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*
 
 from django.conf.urls.defaults import patterns, include, \
-        handler500, handler404, url
+    handler500, handler404, url
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -14,21 +14,10 @@ handler500  # Pyflakes
 urlpatterns = patterns('',
     # app Home
     (r'^', include("cartographie.home.urls")),
-
     # app Formation
     (r'^etablissement/', include("cartographie.formation.urls")),
-
     # app tableau_de_bord
     (r'^dashboard/', include("cartographie.tableau_de_bord.urls")),
-
-    url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset',
-        name='formation_password_reset'
-    ),
-    (r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    (r'^reset/(?P<uidb36>[-\w]+)/(?P<token>[-\w]+)/$',
-        'django.contrib.auth.views.password_reset_confirm'
-    ),
-    (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
     url(r'^admin_tools/', include('admin_tools.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
@@ -36,7 +25,10 @@ urlpatterns = patterns('',
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += patterns('',
+        url(r'^404/$', handler404),
+        url(r'^500/$', handler500),
         url(r'^media/(?P<path>.*)$',
-        'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT, }),
+            'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}
         )
+    )
