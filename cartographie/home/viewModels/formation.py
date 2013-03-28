0e6a501c86bfa488e73ca0a940d31cc28a486551
Mathieu Larose <mathieu.larose@savoirfairelinux.com>
2013-03-28 10:13:23 -0400

    #5851 Ajout du tri

--- cartographie/home/viewModels/formation.py
+++ cartographie/home/viewModels/formation.py
@@ -2,32 +2,49 @@
 
 from cartographie.formation.models import Formation
 from cartographie.home.forms.formation import FormationForm
+from collections import namedtuple
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 
+Column = namedtuple('Column', ['sort_name', 'sort_column', 'name'])
 
 class FormationListeViewModel(object):
     NUM_FORMATIONS_PER_PAGE = 25
+    columns = map(lambda args: Column(*args),
+                  [('nom', 'nom', 'Nom'),
+                   ('niveau', 'niveau_diplome', 'Niveau'),
+                   ('discipline', 'discipline_1__nom', 'Discipline(s)'),
+                   ('etablissement', 'etablissement__nom', 'Établissement'),
+                   ('pays', 'etablissement__pays__nom', 'Pays'),
+                   ('region', 'etablissement__region__nom', 'Région')])
 
+    sort_name2sort_column = dict((col.sort_name, col.sort_column) 
+                                 for col in columns)
+                             
     def __init__(self, request, *args, **kwargs):
         self.form = FormationForm()
         self.formations = Formation.objects.all()
         self.terme_recherche = None
+        self.sort = None
 
-        if FormationListeViewModel._has_query(request):
-            self._filter_by_query(request)
-
+        self._filter(request.GET.get('s'))
+        self._sort(request.GET.get('tri'))
         self._paginate(request)
 
     def get_data(self):
         return {
             "formation_form": self.form,
             "terme_recherche": self.terme_recherche,
-            "formations": self.formations
+            "formations": self.formations,
+            "tri": self.sort,
+            "columns": FormationListeViewModel.columns
         }
 
-    def _filter_by_query(self, request):
-        self.terme_recherche = request.GET["s"]
+    def _filter(self, query):
+        if not query:
+            return
+
+        self.terme_recherche = query
         self.form.initial['s'] = self.terme_recherche
         self.formations = Formation.objects.filter(
             Q(nom__icontains=self.terme_recherche) | 
@@ -38,11 +55,7 @@ class FormationListeViewModel(object):
             Q(etablissement__region__nom__icontains=self.terme_recherche) |
             Q(etablissement__pays__nom__icontains=self.terme_recherche) |
             Q(niveau_diplome__nom__icontains=self.terme_recherche)
-            ).order_by('nom')
-
-    @staticmethod
-    def _has_query(request):
-        return "s" in request.GET
+            )
 
     def _paginate(self, request):
         paginator = Paginator(self.formations, 
@@ -58,3 +71,18 @@ class FormationListeViewModel(object):
         except EmptyPage:
             # If page is out of range (e.g. 9999)
             self.formations = paginator.page(paginator.num_pages)
+
+    def _sort(self, sort):
+        if sort and sort.strip('-') in \
+                FormationListeViewModel.sort_name2sort_column:
+            asc = sort[0] != '-'
+            sort_name = sort.strip('-')
+            prefix = '' if asc else '-'
+            self.sort = { 'asc': asc,
+                          'name': sort_name, 
+                          'string': prefix + sort_name }
+            self.formations = self.formations.order_by(
+                prefix + FormationListeViewModel.
+                sort_name2sort_column[sort_name])
+        else:
+            self.formations = self.formations.order_by('nom')
