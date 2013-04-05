b7ccc1dc88dd490c20f33558e8bce6d7b45b9ae5
David Cormier <david.cormier@savoirfairelinux.com>
2013-04-05 11:48:24 -0400

    [#5811] Couleurs pour statuts
    Utilise des couleurs différentes pour chacun des statuts.
    
    Vert = publiée
    Bleu = validée
    Rouge = Supprimée
    Gris = en rédaction

--- cartographie/formation/templatetags/statusLabel.py
+++ cartographie/formation/templatetags/statusLabel.py
@@ -2,6 +2,7 @@
 
 from django import template
 from cartographie.formation.models.workflow import statusIdToStatusLabel
+from cartographie.formation.constants import statuts_formation as STATUTS
 
 register = template.Library()
 
@@ -14,3 +15,19 @@ def statusLabel(value):
         choice du abstract model WorkflowMixin
     """
     return statusIdToStatusLabel(value)
+
+@register.filter
+def statusClass(value):
+    """
+        Optient la classe css qui doit être associée à un statut numérique
+    """
+    if value == STATUTS.publiee:
+        return 'label-success'
+    if value == STATUTS.supprimee:
+        return 'label-important'
+    if value == STATUTS.en_redaction:
+        return ''
+    if value == STATUTS.validee:
+        return 'label-info'
+    return ''
+
