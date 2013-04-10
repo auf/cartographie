9ee4110a4efbdff093ed560d0e2940ba1aabfc3d
Mathieu Larose <mathieu.larose@savoirfairelinux.com>
2013-04-10 17:07:50 -0400

    [#5775] Affiche les valeurs actives seulement.

---
+++ cartographie/formation/templatetags/actif.py
@@ -0,0 +1,47 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def composantes_actives(formation):
+    return formation.etablissement_composante.filter(actif=True)
+
+@register.filter
+def contacts_actifs(formation):
+    return formation.contacts.filter(actif=True)
+
+@register.filter
+def disciplines_actives(formation):
+    def discipline2nom(discipline):
+        return discipline.nom
+
+    def index2discipline(i):
+        return getattr(formation, 'discipline_' + str(i))
+
+    def is_active(obj):
+        return obj.actif
+
+    return ', '.join(map(discipline2nom,
+                         filter(is_active,
+                                map(index2discipline,
+                                    range(1,4)))))
+
+@register.filter
+def partenaires_auf_actifs(formation):
+    return formation.partenaires_auf.filter(actif=True)
+
+@register.filter
+def langues_actives(formation):
+    return formation.langue.filter(actif=True)
+
+@register.filter
+def niveaux_entrees_actifs(formation):
+    return formation.niveau_entree.filter(actif=True)
+
+@register.filter
+def responsables_actifs(formation):
+    return formation.responsables.filter(actif=True)
+
+@register.filter
+def vocations_actives(formation):
+    return formation.vocation.filter(actif=True)
