0dc2137fca0ff8739589bf848562525d345f234e
Mathieu Larose <mathieu.larose@savoirfairelinux.com>
2013-04-19 10:15:14 -0400

    Suppression du champs de recherche sur la page d'accueil.

--- cartographie/home/viewModels/accueil.py
+++ cartographie/home/viewModels/accueil.py
@@ -1,15 +1,8 @@
 #coding: utf-8
 
-from cartographie.home.forms.formation import FormationForm
-
-
 class AccueilViewModel(object):
-    form = FormationForm()
-
     def __init__(self, request, *args, **kwargs):
         pass
 
     def get_data(self):
-        return {
-            "formation_form": self.form,
-        }
+        return {}
