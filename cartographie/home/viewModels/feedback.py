356b93fd617339755b10bda0d134ad54c39fe9e1
Mathieu Larose <mathieu.larose@savoirfairelinux.com>
2013-04-22 16:15:50 -0400

    [#6084] Envoie un courriel

---
+++ cartographie/home/viewModels/feedback.py
@@ -0,0 +1,47 @@
+#coding: utf-8
+
+from cartographie.home.forms.feedback import FeedbackForm
+from django.conf import settings
+from django.contrib import messages
+from django.core.mail import EmailMessage
+from django.template import Context
+from django.template.loader import get_template
+
+class FeedbackViewModel(object):
+    def __init__(self, request, *args, **kwargs):
+        self.form = FeedbackForm()
+
+        if request.method == 'POST':
+            self.form = FeedbackForm(request.POST)
+            if self.form.is_valid():
+                feedback = self.form.save()
+                if self._envoyer_courriel(feedback):
+                    messages.success(
+                        request, u"Votre message nous a bien été envoyé. Merci!"
+                        )
+                    self.form = FeedbackForm()
+                else:
+                    messages.error(
+                        request, u"Votre message n'a pu être envoyé."
+                        )
+
+    def _envoyer_courriel(self, feedback):
+        plaintext = get_template('cartographie/email/feedback.txt')
+
+        subject = "AUF - Cartographie: Nouveau feedback"
+
+        context = Context({
+            'feedback': feedback
+            })
+
+        text_content = plaintext.render(context)
+        from_email = 'cartographie@auf.org'
+        to = settings.EMAIL_FEEDBACK
+
+        msg = EmailMessage(subject, text_content, from_email, to)
+        return msg.send()
+
+    def get_data(self):
+        return {
+            'form': self.form,
+            }
