008bbb62aa212ce6647e6173c094996c2ea150b9
David Cormier <david.cormier@savoirfairelinux.com>
2013-04-22 16:36:41 -0400

    [#5928] Ajoute le module de stats

---
+++ cartographie/formation/stats.py
@@ -0,0 +1,32 @@
+
+from    __future__ import print_function
+from   auf.django.references.models import Etablissement
+from   django.db.models import Count
+from   formation.models import Formation
+
+
+def num_etablissements_per_country():
+
+    def result2pair(result):
+        return result['pays__code_iso3'].lower(), result['count']
+
+    query = Etablissement.objects.values('pays__code_iso3')\
+        .annotate(count=Count('pays__code_iso3'))
+
+    return dict(map(result2pair, query))
+
+
+def num_formations_per_country():
+
+    def result2pair(result):
+        return (result['etablissement__pays__code_iso3'].lower(),
+                result['count'])
+
+    # On doit passer par Formation.objects parce que
+    # 'related_name' = '+' dans EtablissementBase pour la colonne
+    # 'pays'.
+
+    query = Formation.objects.values('etablissement__pays__code_iso3')\
+        .annotate(count=Count('etablissement__pays__code_iso3'))
+
+    return dict(map(result2pair, query))
