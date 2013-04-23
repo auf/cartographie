dd93b9b742ff073c3b7c797058da09a6a3e6804e
David Cormier <david.cormier@savoirfairelinux.com>
2013-04-23 10:32:04 -0400

    [#6085] Ajoute l'en-tête de navigation
    Récupère le style depuis le template et le mets dans le css.
    Ajoute une navigation 'nav-pills' de twitter-bootstrap.

--- cartographie/formation/templatetags/actif.py
+++ cartographie/formation/templatetags/actif.py
@@ -2,6 +2,13 @@ from django import template
 
 register = template.Library()
 
+@register.simple_tag
+def active(request, pattern):
+    import re
+    if re.search(pattern, request.path):
+        return 'active'
+    return ''
+
 @register.filter
 def composantes_actives(formation):
     return formation.etablissement_composante.filter(actif=True)
