ee8ca8d7aac7af5f9f5b8df411756a90cd4b193f
Alexandre Boily <alexandre.boily@savoirfairelinux.com>
2013-11-20 09:53:38 -0500

    Transitions vers "en rédaction" par référent

--- cartographie/formation/workflow.py
+++ cartographie/formation/workflow.py
@@ -1,30 +1,30 @@
-#coding: utf-8
-from cartographie.formation.constants import statuts_formation as STATUTS, statut2label
-from collections import defaultdict
+# -*- coding: utf-8 -*-
+
+from cartographie.formation.constants import statuts_formation as STATUTS
 
 # Admin is implicit.
 TRANSITIONS = {
     STATUTS.en_redaction: {
-        STATUTS.supprimee: { 'roles': ['token'] },
-        STATUTS.validee: {'roles': ['token'] },
+        STATUTS.supprimee: {'roles': ['token']},
+        STATUTS.validee: {'roles': ['token']},
         },
 
     STATUTS.validee: {
-        STATUTS.publiee: {'roles': ['editeur'] },
+        STATUTS.publiee: {'roles': ['editeur']},
         STATUTS.en_redaction: {'retro': True,
-                               'roles': ['editeur', 'token'] },
-        STATUTS.supprimee: {'roles': ['token'] },
+                               'roles': ['editeur', 'referent', 'token']},
+        STATUTS.supprimee: {'roles': ['token']},
         },
-    
+
     STATUTS.publiee: {
         STATUTS.validee: {'retro': True,
-                          'roles': ['editeur'] },
+                          'roles': ['editeur']},
         STATUTS.en_redaction: {'retro': True,
-                               'roles': ['editeur'] },
-        STATUTS.supprimee: {'roles': [] },
+                               'roles': ['editeur', 'referent']},
+        STATUTS.supprimee: {'roles': []},
         },
-    
+
     STATUTS.supprimee: {
-        STATUTS.en_redaction: {'roles': [] }
+        STATUTS.en_redaction: {'roles': []}
         }
     }
