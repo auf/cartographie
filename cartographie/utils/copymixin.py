093b134298068c47951324d739f990265662e319
Alexandre Boily <alexandre.boily@savoirfairelinux.com>
2013-11-25 14:19:55 -0500

    Version lente du deepcopy

--- cartographie/utils/copymixin.py
+++ cartographie/utils/copymixin.py
@@ -3,6 +3,14 @@
 from copy import copy, deepcopy
 
 from django.db.models.manager import Manager
+from django.db.models import related
+
+
+# List of related field types, cast to a tuple for further isinstance() usage
+FIELD_TYPES = tuple([
+    getattr(related, x)
+    for x in dir(related)
+    if isinstance(getattr(related, x), type)])
 
 
 class CopyMixin(object):
@@ -10,29 +18,42 @@ class CopyMixin(object):
     '''Mixin de copie personnalisé pour Django'''
 
     def __deepcopy__(self, memo):
+        class_dict = self.__class__.__dict__
+        fields = set([
+            k
+            for k, v in class_dict.items()
+            if isinstance(v, FIELD_TYPES)])
+
+        if 'content_fields' in class_dict:
+            content_fields = set(self.content_fields)
+            fields.union(content_fields)
+
         new = copy(self)
         new.pk = None
         new.save()
 
         print(self.__class__.__name__)
 
-        if 'content_fields' in self.__class__.__dict__:
-            for attr in self.content_fields:
-                other = getattr(self, attr)
-                print('\t%s - %s' % (other, other.__class__.__name__))
-                if isinstance(other, Manager):
+        for name in fields:
+            print('\t%s' % (name, ))
+            orig_field = getattr(self, name)
 
-                    new_other = [deepcopy(instance) for instance in other.all()]
+            if isinstance(orig_field, Manager):
+                print('\t\tmanager')
+                if name.endswith('_set'):
+                    print('\t\t\t_set')
+                    new_field = copy(orig_field)
+                    for val in orig_field.all():
+                        new_val = copy(val)
+                        new_field.add(new_val)
+                    setattr(new, name, new_field.all())
+            elif isinstance(orig_field, CopyMixin):
+                new_field = deepcopy(orig_field)
+                setattr(new, name, new_field)
+            else:
+                new_field = orig_field
+                setattr(new, name, new_field)
 
-                    
-                elif isinstance(other, CopyMixin):
-                    print('\t\tCopyMixin')
-                    new_other = deepcopy(other)
-                else:
-                    new_other = other
-
-                new.__setattr__(attr, new_other)
-
-            new.save()
-
+        new.save()
+        
         return new
